ab4f62698d197dd731a492a772c8a3ff45ba301c
eranda97 <epremarathna@gmail.com>
2024-02-20 20:19:57 +0530

    finished assignment 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -1,4 +1,4 @@
-#include "unity.h"
+#include "../../assignment-autotest/Unity/src/unity.h"
 #include <stdbool.h>
 #include <stdlib.h>
 #include "../../examples/autotest-validate/autotest-validate.h"
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+
+    // my_username();
+    // malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(),malloc_username_from_conf_file(),"username ok");
+    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
